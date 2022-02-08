05efd02442b922fbc464edd2dcca0b46f96fda97
Blink <tipsyblink11@gmail.com>
2022-02-07 18:16:47 -0800

    update

--- Program.cs
+++ Program.cs
@@ -9,9 +9,11 @@ namespace BankProgram
         {
             Person person = new Person("Metro Bank", "John");
             Transaction john = new Transaction(AccountType.Investment_Individual, person);
+            john.InitialBalance();
             john.Withdraw(500);
             john.CheckStatus();
             john.Deposit(3000);
+            john.CheckStatus();
             john.Withdraw(600);
             john.CheckStatus();
         }
@@ -49,7 +51,6 @@ namespace BankProgram
                 this.accountType = accountType;
                 this.BankName = person.BankName;
                 this.OwnerName = person.OwnerName;
-                Console.WriteLine($"Initial Balance {balance}");
             }
 
             public void Deposit(int amount)
@@ -76,20 +77,21 @@ namespace BankProgram
                         else
                         {
                             balance -= amount;
+                            Console.WriteLine($"Amount Withdrawed: {amountWithrawed}");
                         }
                     }
                     else
                     {
                         balance -= amount;
+                        Console.WriteLine($"Amount Withdrawed: {amountWithrawed}");
                     }
-                }
-               
+                }             
             }
 
             public int Transfer(int amount)
             {
                 //I'm not sure what functionality the transfer will do,
-                //I would just assume it means transfer money
+                //I'm just going to assume it means transfer money
                 return balance -= amount;              
             }
 
@@ -97,6 +99,11 @@ namespace BankProgram
             {
                 Console.WriteLine($"Bank Name: {BankName}");
                 Console.WriteLine($"{OwnerName} Your Balance is : {balance}");
+            }
+
+            public void InitialBalance()
+            {
+                Console.WriteLine($"{OwnerName} Initial Balance : {balance}");
             }
         }
     }
